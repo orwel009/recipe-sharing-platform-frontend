40e497b4dfaa00ed49164bb8c485b9cb77459ba1
orwel009 <orwelvincent1@gmail.com>
2025-11-17 13:51:56 +0530

    Basic Home navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
